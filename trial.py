8ddb4c946b1fbd28c91949b45ac8b87939ab1d40
DinnerDinner <singhpranjwal@gmail.com>
2026-01-05 20:38:24 -0500

    AB#52 bs4

--- trial.py
+++ trial.py
@@ -1,6 +1,7 @@
 import httpx
 from reportlab.lib.pagesizes import LETTER
 from reportlab.pdfgen import canvas
+from bs4 import BeautifulSoup
 
 r = httpx.get('https://www.linkedin.com/jobs/view/4354624586/')
 
@@ -12,17 +13,23 @@ max_chars = 90
 font_size = 8
 line_height = font_size+3
 c.setFont("Helvetica", font_size)
-for line in r.text.split('\n'):
+soup = BeautifulSoup(r.text, 'html.parser')
+soup = soup.find("div", class_="description__text description__text--rich")
+soup = soup.get_text()
+
+for line in soup.split('\n'):
     if line.strip()== "":
         continue
     
-    for i in range(0, len(line), max_chars): 
+    for i in range(0, len(line), max_chars):
+        c.setFont("Helvetica", font_size)
+ 
         if y < 50:
             c.showPage()   
             y = height-50
-            c.setFont("Helvetica", font_size)
             
         c.drawString(x, y, line[i: i+max_chars])
         y -= line_height
         
 c.save()
+
