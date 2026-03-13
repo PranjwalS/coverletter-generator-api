fd2c792e09afdc119ef1d8e0cbe34b4ffeafe043
DinnerDinner <singhpranjwal@gmail.com>
2026-03-12 23:27:08 -0400

    just finished both midterms, will make extensin for autofill tmrw

---
+++ job-apply-extension/content.js
@@ -0,0 +1,17 @@
+const inputs = document.querySelectorAll("input, textarea");
+
+inputs.forEach(input => {
+
+  const name = input.name.toLowerCase();
+
+  if(name.includes("email")){
+    input.value = "user@email.com";
+  }
+
+  if(name.includes("phone")){
+    input.value = "1234567890";
+  }
+
+});
+
+///gotta move everything to a storage with a collection, key-value typeshit (HIDDEN)
---
+++ job-apply-extension/popup.js
@@ -0,0 +1,12 @@
+document.getElementById("autofill").onclick = () => {
+
+  chrome.tabs.query({active:true,currentWindow:true}, tabs => {
+
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      {action:"autofill"}
+    );
+
+  });
+
+};
