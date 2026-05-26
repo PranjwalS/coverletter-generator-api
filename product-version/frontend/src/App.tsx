e26c9b5a0ec89970529e1e79a969a7d4b5bd6127
DinnerDinner <singhpranjwal@gmail.com>
2026-05-25 23:40:52 -0400

    refactored routing for all pages

--- product-version/frontend/src/App.tsx
+++ product-version/frontend/src/App.tsx
@@ -2,7 +2,13 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import DashboardPage from "./pages/DashboardPage";
+import DashboardLayout from "./pages/DashboardLayout";
+import OverviewPage from "./pages/OverviewPage";
+import MasterDashboardPage from "./pages/MasterDashboardPage";
+import CareerTwinInfoPage from "./pages/CareerTwinInfoPage";
+import CareerTwinProfilePage from "./pages/CareerTwinProfilePage";
+import CVPage from "./pages/CVPage";
+import CoverLetterPage from "./pages/CoverLetterPage";
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
   const token = localStorage.getItem("token");
@@ -16,16 +22,61 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+
+        {/* All dashboard routes share the sidebar layout */}
         <Route
           path="/dashboard"
           element={
             <PrivateRoute>
-              <DashboardPage />
+              <DashboardLayout />
             </PrivateRoute>
           }
-        />
+        >
+          <Route index element={<Navigate to="overview" replace />} />
+          <Route path="overview" element={<OverviewPage />} />
+          <Route path="jobs" element={<MasterDashboardPage />} />
+          <Route path="careertwin/profile" element={<CareerTwinProfilePage />} />
+          <Route path="careertwin/info" element={<CareerTwinInfoPage />} />
+          <Route path="products/cv" element={<CVPage />} />
+          <Route path="products/coverletter" element={<CoverLetterPage />} />
+        </Route>
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+
+
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import LandingPage from "./pages/LandingPage";
+// import LoginPage from "./pages/LoginPage";
+// import SignupPage from "./pages/SignupPage";
+// import DashboardPage from "./pages/DashboardPage";
+
+// function PrivateRoute({ children }: { children: React.ReactNode }) {
+//   const token = localStorage.getItem("token");
+//   return token ? <>{children}</> : <Navigate to="/login" replace />;
+// }
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<LandingPage />} />
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/signup" element={<SignupPage />} />
+//         <Route
+//           path="/dashboard"
+//           element={
+//             <PrivateRoute>
+//               <DashboardPage />
+//             </PrivateRoute>
+//           }
+//         />
+//         <Route path="*" element={<Navigate to="/" replace />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
